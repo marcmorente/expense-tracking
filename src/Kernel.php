c77f461eeb0e552d99e310447f89bc1bbb839e52
Marc Morente <marc.morente@gmail.com>
2026-07-25 13:39:19 +0200

    fix files to pass php tooling checks

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,19 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+/** Application kernel. */
+final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    /**
-     * @return list<string> An array of allowed values for APP_ENV
-     */
-    private function getAllowedEnvs(): array
-    {
-        return ['prod', 'dev', 'test'];
-    }
 }
