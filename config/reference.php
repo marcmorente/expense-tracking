20533a91877b62d2cd9ef68209e87dcc2e9afbdc
Marc Morente <marc.morente@gmail.com>
2026-07-28 15:08:47 +0200

    restore generated config reference

--- config/reference.php
+++ config/reference.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 // This file is auto-generated and is for apps only. Bundles SHOULD NOT rely on its content.
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
@@ -1061,7 +1059,9 @@ final class App
     public static function config(array $config): array
     {
         /** @var ConfigType $config */
-        return AppReference::config($config);
+        $config = AppReference::config($config);
+
+        return $config;
     }
 }
 
